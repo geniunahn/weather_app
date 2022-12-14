a9fafe72bfaf4a3b5ab4d94189d736a3a94a5e8d
geniunahn <106502672+geniunahn@users.noreply.github.com>
2022-12-14 23:35:21 +0900

    Add files via upload
    - 서울, 안양, 대구, 부산, 제주도 다섯 지역의 날씨 정보를 오픈웨더의 api로 구현함.
    - 시간과 요일은 현재 시간을 반영하여 실시간으로 변하도록 구현함.
    - 해당 지역의 날씨에 맞게 배경 동영상이 반응하여 재생하도록 구현함.
    - 날씨 조언은 랜덤하게 나오도록 구현함.

---
+++ src/js/component/component.js
@@ -0,0 +1,198 @@
+// 네비 스크롤 이벤트
+
+$(function () {
+  $(window).scroll(function () {
+    let scrollT2 = $(window).scrollTop();
+
+    if (scrollT2 > 1) {
+      $(".nav").addClass("active");
+    } else {
+      $(".nav").removeClass("active");
+    }
+  });
+});
+
+// 시티 버튼 이벤트
+
+$(function () {
+  $(".nav i").on("click", function () {
+    $(".city_box").removeClass("close");
+    $(".city_box").addClass("active");
+  });
+
+  $(".city_box_btn_box i").on("click", function () {
+    $(".city_box").removeClass("active");
+    $(".city_box").addClass("close");
+  });
+});
+
+$(function () {
+  // 어드바이스 랜덤
+  const advice = [
+    {
+      quote: "춥고 건조해 감기 걸리기 쉬운 날입니다. 빙판 길을 조심하세요.",
+    },
+    {
+      quote:
+        "동장군의 기세가 이어지는 겨울밤 난방기기 사용 시 화재 주의하세요!",
+    },
+    {
+      quote:
+        "한파가 지속되므로 야외활동은 가급적 자제하시고 방한 용품을 꼭 착용해주세요.",
+    },
+    {
+      quote:
+        "한파로 인하여 계량기 동파와 미끄럼 사고가 자주 발생하니 각별히 유의하세요.",
+    },
+    {
+      quote:
+        "비 또는 눈이 얼어 도로가 미끄러운 곳이 많으니 대중교통을 이용해주세요.",
+    },
+  ];
+
+  const todayAdvice = advice[Math.floor(Math.random() * advice.length)];
+  const todayAdviceEl = document.querySelector(".today_box_advice_text");
+
+  todayAdviceEl.innerHTML = todayAdvice.quote;
+
+  // 시간과 요일
+  function Clock() {
+    let now = new Date();
+
+    // 시간
+    (pe = "오전"), (hou = now.getHours());
+
+    if (hou == 0) {
+      hou = 12;
+    }
+    if (hou > 12) {
+      hou = hou - 12;
+      pe = "오후";
+    }
+
+    let time1 = `${pe} ${hou + 1}시`;
+    let time1El = document.querySelector(".time1");
+    time1El.innerHTML = time1;
+
+    let time2 = `${pe} ${hou + 2}시`;
+    let time2El = document.querySelector(".time2");
+    time2El.innerHTML = time2;
+
+    let time3 = `${pe} ${hou + 3}시`;
+    let time3El = document.querySelector(".time3");
+    time3El.innerHTML = time3;
+
+    let time4 = `${pe} ${hou + 4}시`;
+    let time4El = document.querySelector(".time4");
+    time4El.innerHTML = time4;
+
+    // 요일
+    dname = now.getDay();
+
+    let week = [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ];
+
+    // 1번
+    let addNum1 = 1;
+    let addDname1 = dname + addNum1;
+
+    if (addDname1 >= 7) {
+      addDname1 = dname + addNum1 - 7;
+    }
+
+    let weekDname1 = week[addDname1];
+    let dnameEL1 = document.querySelector(".dname1");
+    dnameEL1.innerHTML = weekDname1;
+    let dnameEL1_1 = document.querySelector(".dname1_1");
+    dnameEL1_1.innerHTML = weekDname1;
+
+    // 2번
+    let addNum2 = 2;
+    let addDname2 = dname + addNum2;
+
+    if (addDname2 >= 7) {
+      addDname2 = dname + addNum2 - 7;
+    }
+
+    let weekDname2 = week[addDname2];
+    let dnameEL2 = document.querySelector(".dname2");
+    dnameEL2.innerHTML = weekDname2;
+    let dnameEL2_1 = document.querySelector(".dname2_1");
+    dnameEL2_1.innerHTML = weekDname2;
+
+    // 3번
+    let addNum3 = 3;
+    let addDname3 = dname + addNum3;
+
+    if (addDname3 >= 7) {
+      addDname3 = dname + addNum3 - 7;
+    }
+
+    let weekDname3 = week[addDname3];
+    let dnameEL3 = document.querySelector(".dname3");
+    dnameEL3.innerHTML = weekDname3;
+    let dnameEL3_1 = document.querySelector(".dname3_1");
+    dnameEL3_1.innerHTML = weekDname3;
+
+    // 4번
+    let addNum4 = 4;
+    let addDname4 = dname + addNum4;
+
+    if (addDname4 >= 7) {
+      addDname4 = dname + addNum4 - 7;
+    }
+    let weekDname4 = week[addDname4];
+    let dnameEL4 = document.querySelector(".dname4");
+    dnameEL4.innerHTML = weekDname4;
+    let dnameEL4_1 = document.querySelector(".dname4_1");
+    dnameEL4_1.innerHTML = weekDname4;
+
+    // 5번
+    let addNum5 = 5;
+    let addDname5 = dname + addNum5;
+
+    if (addDname5 >= 7) {
+      addDname5 = dname + addNum5 - 7;
+    }
+    let weekDname5 = week[addDname5];
+    let dnameEL5 = document.querySelector(".dname5");
+    dnameEL5.innerHTML = weekDname5;
+    let dnameEL5_1 = document.querySelector(".dname5_1");
+    dnameEL5_1.innerHTML = weekDname5;
+
+    // 6번
+    let addNum6 = 6;
+    let addDname6 = dname + addNum6;
+
+    if (addDname6 >= 7) {
+      addDname6 = dname + addNum6 - 7;
+    }
+    let weekDname6 = week[addDname6];
+    let dnameEL6 = document.querySelector(".dname6");
+    dnameEL6.innerHTML = weekDname6;
+    let dnameEL6_1 = document.querySelector(".dname6_1");
+    dnameEL6_1.innerHTML = weekDname6;
+
+    // 7번
+    let addNum7 = 7;
+    let addDname7 = dname + addNum7;
+
+    if (addDname7 >= 7) {
+      addDname7 = dname + addNum7 - 7;
+    }
+    let weekDname7 = week[addDname7];
+    let dnameEL7 = document.querySelector(".dname7");
+    dnameEL7.innerHTML = weekDname7;
+    let dnameEL7_1 = document.querySelector(".dname7_1");
+    dnameEL7_1.innerHTML = weekDname7;
+  }
+
+  Clock();
+});
---
+++ src/js/index.js
@@ -0,0 +1,299 @@
+const API_KEY = "74efd065a24c76dd62c5bc7e7757be2b";
+
+function getWeatherData(cityname) {
+  city_name = cityname;
+  let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
+
+  fetch(API_URL)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      // 도시명
+      let name = data.name;
+      let korName = nameEngToKor(name);
+      let korNameEl = document.querySelector(".main_box_text_2");
+      korNameEl.innerHTML = korName;
+      function nameEngToKor(n_en) {
+        var n_en_arr = [
+          "Seoul",
+          "Anyang-si",
+          "Incheon",
+          "Daegu",
+          "Busan",
+          "Jeju City",
+        ];
+        var n_kor_arr = ["서울", "안양", "인천", "대구", "부산", "제주도"];
+        for (var i = 0; i < n_en_arr.length; i++) {
+          if (n_en_arr[i] == n_en) {
+            return n_kor_arr[i];
+            break;
+          }
+        }
+        return "none";
+      }
+
+      //  현재 온도
+      let temp = parseInt(data.main.temp - 273.15);
+      let tempEl = document.querySelector(".main_box_text_3");
+      let tempEl_2 = document.querySelector(".today_box_info_temp_text");
+      tempEl.innerHTML = `${temp}&deg`;
+      tempEl_2.innerHTML = `${temp}&deg`;
+
+      // 날씨 상태 설명
+      let desc = data.weather[0].id;
+      let korDesc = wDescEngToKor(desc);
+      let korDesEl = document.querySelector(".main_box_text_4");
+      korDesEl.innerHTML = korDesc;
+      function wDescEngToKor(w_id) {
+        var w_id_arr = [
+          201, 200, 202, 210, 211, 212, 221, 230, 231, 232, 300, 301, 302, 310,
+          311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511, 520, 521, 522,
+          531, 600, 601, 602, 611, 612, 615, 616, 620, 621, 622, 701, 711, 721,
+          731, 741, 751, 761, 762, 771, 781, 800, 801, 802, 803, 804, 900, 901,
+          902, 903, 904, 905, 906, 951, 952, 953, 954, 955, 956, 957, 958, 959,
+          960, 961, 962,
+        ];
+        var w_kor_arr = [
+          "가벼운 비를 동반한 천둥구름",
+          "비를 동반한 천둥구름",
+          "폭우를 동반한 천둥구름",
+          "약한 천둥구름",
+          "천둥구름",
+          "강한 천둥구름",
+          "불규칙적 천둥구름",
+          "약한 연무를 동반한 천둥구름",
+          "연무를 동반한 천둥구름",
+          "강한 안개비를 동반한 천둥구름",
+          "가벼운 안개비",
+          "안개비",
+          "강한 안개비",
+          "가벼운 적은비",
+          "적은비",
+          "강한 적은비",
+          "소나기와 안개비",
+          "강한 소나기와 안개비",
+          "소나기",
+          "악한 비",
+          "중간 비",
+          "강한 비",
+          "매우 강한 비",
+          "극심한 비",
+          "우박",
+          "약한 소나기 비",
+          "소나기 비",
+          "강한 소나기 비",
+          "불규칙적 소나기 비",
+          "가벼운 눈",
+          "눈",
+          "폭설",
+          "진눈깨비",
+          "소나기 진눈깨비",
+          "약한 비와 눈",
+          "비와 눈",
+          "약한 소나기 눈",
+          "소나기 눈",
+          "강한 소나기 눈",
+          "박무",
+          "연기",
+          "연무",
+          "모래 먼지",
+          "안개",
+          "모래",
+          "먼지",
+          "화산재",
+          "돌풍",
+          "토네이도",
+          "구름 한 점 없는 맑은 하늘",
+          "약간의 구름이 낀 하늘",
+          "드문드문 구름이 낀 하늘",
+          "구름이 거의 없는 하늘",
+          "구름으로 뒤덮인 흐린 하늘",
+          "토네이도",
+          "태풍",
+          "허리케인",
+          "한랭",
+          "고온",
+          "바람부는",
+          "우박",
+          "바람이 거의 없는",
+          "약한 바람",
+          "부드러운 바람",
+          "중간 세기 바람",
+          "신선한 바람",
+          "센 바람",
+          "돌풍에 가까운 센 바람",
+          "돌풍",
+          "심각한 돌풍",
+          "폭풍",
+          "강한 폭풍",
+          "허리케인",
+        ];
+        for (var i = 0; i < w_id_arr.length; i++) {
+          if (w_id_arr[i] == w_id) {
+            return w_kor_arr[i];
+            break;
+          }
+        }
+        return "none";
+      }
+
+      //  최대 기온, 최저 기온
+      let tempMax = parseInt(data.main.temp_max - 273.15);
+      let tempMin = parseInt(data.main.temp_min - 273.15);
+      let tempMaxEl = document.querySelector(".main_box_text_6");
+      let tempMinEl = document.querySelector(".main_box_text_7");
+      tempMaxEl.innerHTML = `H:${tempMax}&deg`;
+      tempMinEl.innerHTML = `L:${tempMin}&deg`;
+
+      // 날씨 아이콘
+      let wIconEl = document.querySelector(".today_img");
+      let weather_icon = data.weather[0].icon;
+      let w_icon_src = wIcon(weather_icon);
+      wIconEl.src = w_icon_src;
+      function wIcon(weather_icon) {
+        var w_icon_arr = [
+          "01d",
+          "01n",
+          "02d",
+          "02n",
+          "03d",
+          "03n",
+          "04d",
+          "04n",
+          "09d",
+          "09n",
+          "10d",
+          "10n",
+          "11d",
+          "11n",
+          "13d",
+          "13n",
+          "50d",
+          "50n",
+        ];
+        var w_icon_src_arr = [
+          "./src/images/clear_sky_d.svg",
+          "./src/images/clear_sky_n.svg",
+          "./src/images/few_clouds_d.svg",
+          "./src/images/few_clouds_n.svg",
+          "./src/images/scattered_clouds_d.svg",
+          "./src/images/scattered_clouds_n.svg",
+          "./src/images/broken_clouds_d.svg",
+          "./src/images/broken_clouds_n.svg",
+          "./src/images/shower_rain_d.svg",
+          "./src/images/shower_rain_n.svg",
+          "./src/images/rain_d.svg",
+          "./src/images/rain_n.svg",
+          "./src/images/thunderstorm_d.svg",
+          "./src/images/thunderstorm_n.svg",
+          "./src/images/snow_d.svg",
+          "./src/images/snow_n.svg",
+          "./src/images/mist_d.svg",
+          "./src/images/mist_n.svg",
+        ];
+        for (var i = 0; i < w_icon_arr.length; i++) {
+          if (w_icon_arr[i] == weather_icon) {
+            return w_icon_src_arr[i];
+            break;
+          }
+        }
+        return "none";
+      }
+
+      // 일출, 일몰
+      let sunRiseEl = document.querySelector(".sunrise_text");
+      let sunRise = data.sys.sunrise;
+      let sunRiseTrans = Unix_timestamp(sunRise);
+      sunRiseEl.innerHTML = `${sunRiseTrans}`;
+
+      let sunSetEl = document.querySelector(".sunset_text");
+      let sunSet = data.sys.sunset;
+      let sunSetTrans = Unix_timestamp(sunSet);
+      sunSetEl.innerHTML = `${sunSetTrans}`;
+
+      function Unix_timestamp(t) {
+        let date = new Date(t * 1000);
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let pe = "오전";
+        if (hour == 0) {
+          hour = 12;
+        }
+        if (hour > 12) {
+          hour = hour - 12;
+          pe = "오후";
+        }
+        return `${pe} ${hour}:${minute}`;
+      }
+
+      // 습도
+      let humidity = data.main.humidity;
+      let humidityEl = document.querySelector(".humidity");
+      humidityEl.innerHTML = `${humidity}%`;
+
+      // 풍속
+      let wind = data.wind.speed;
+      let windEl = document.querySelector(".wind");
+      windEl.innerHTML = `${wind}m/s`;
+
+      // 날씨 배경
+      let video_src = video(weather_icon);
+      console.log(video_src);
+      const videoEl = document.querySelector(".video");
+      videoEl.src = video_src;
+      console.log(videoEl.src);
+
+      function video(weather) {
+        var video_arr = [
+          "01d",
+          "01n",
+          "02d",
+          "02n",
+          "03d",
+          "03n",
+          "04d",
+          "04n",
+          "09d",
+          "09n",
+          "10d",
+          "10n",
+          "11d",
+          "11n",
+          "13d",
+          "13n",
+          "50d",
+          "50n",
+        ];
+        var video_src_arr = [
+          "./src/video/clear_sky.mp4",
+          "./src/video/clear_sky.mp4",
+          "./src/video/few_clouds.mp4",
+          "./src/video/few_clouds.mp4",
+          "./src/video/scattered_clouds.mp4",
+          "./src/video/scattered_clouds.mp4",
+          "./src/video/broken_clouds.mp4",
+          "./src/video/broken_clouds.mp4",
+          "./src/video/shower_rain.mp4",
+          "./src/video/shower_rain.mp4",
+          "./src/video/rain.mp4",
+          "./src/video/rain.mp4",
+          "./src/video/thunderstorm.mp4",
+          "./src/video/thunderstorm.mp4",
+          "./src/video/snow.mp4",
+          "./src/video/snow.mp4",
+          "./src/video/mist.mp4",
+          "./src/video/mist.mp4",
+        ];
+        for (var i = 0; i < video_arr.length; i++) {
+          if (video_arr[i] == weather) {
+            return video_src_arr[i];
+            break;
+          }
+        }
+        return "none";
+      }
+    });
+}
+getWeatherData("anyang-si");
